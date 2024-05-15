23c3030b877a5f57021eff1f3a533adc78f62aae
KangHyun Lee <146345326+totokangto@users.noreply.github.com>
2024-05-16 01:39:24 +0900

    resnet20

---
+++ resnet.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class IdentityShortcut(nn.Module):
+ 
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.add_channels = out_channels - in_channels
+    
+    def forward(self, x):
+        out = F.pad(x, (0, 0, 0, 0, 0, self.add_channels),'constant',0)
+        out = F.max_pool2d(out,1,stride=2)
+        # out = F.max_pool2d(out,2)
+        return out
+
+class BuildingBlock(nn.Module):
+
+    def __init__(self, in_channels, out_channels, stride):
+        super(BuildingBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        # stride is always 1 in middle of building block
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3,
+                               stride=1, padding="same", bias=False)   
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()       
+        # when dimensions increase, use zero padding  
+        if stride != 1 :
+            self.shortcut = IdentityShortcut(in_channels,out_channels)
+        
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNet20(nn.Module):
+    def __init__(self, block, num_blocks, num_classes= 10 ):
+        super(ResNet20, self).__init__()
+        # input channels = 16
+        self.in_channels = 16
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3,
+                               stride=1, padding="same", bias=False)
+        self.bn = nn.BatchNorm2d(16)
+
+        self.conv2 = self._make_layer(block, 16, num_blocks[0], stride=1)
+        self.conv3 = self._make_layer(block, 32, num_blocks[1], stride=2)
+        self.conv4 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        self.linear = nn.Linear(64, num_classes) # input : depth*W*H
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        # Downsampling is performed at conv3_1 and conv4_1
+        # with a stride of 2
+        layers = [block(self.in_channels, out_channels, stride)]
+        # change in_channels value 
+        # so that input channels of next layer 
+        # equal to output channels of prior layer
+        self.in_channels = out_channels
+        # the other layers in building block have stride of 1
+        for i in range(num_blocks-1):
+            layers.append(block(self.in_channels, out_channels, 1))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = F.avg_pool2d(out,8)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+def resnet20():
+    return ResNet20(BuildingBlock, [3, 3, 3])
+def test():
+    net = resnet20()
+    y = net(torch.randn(1, 3, 32, 32))
+    print(y.size())
---
+++ train.py
@@ -0,0 +1,116 @@
+'''Train CIFAR10 with PyTorch.'''
+from torch.utils.tensorboard import SummaryWriter
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+from torchsummary import summary
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+from os import path
+import argparse
+
+import resnet
+
+# Data
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(
+    root='/local_datasets/cifar_10', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=128, shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(
+    root='./local_datasets/cifar_10', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+net = resnet.resnet20()
+net = net.to(device)
+summary(net,(3,32,32))
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.1,
+                      momentum=0.9, weight_decay=1e-4)
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,[32000,48000], gamma=0.1)
+
+
+# Training
+writer = SummaryWriter(path.join('log', 'train'), flush_secs=1)
+def train(epoch,iter):
+    print(f"Epoch : {epoch}")
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    i = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+        iter += 1
+    writer.add_scalar("Loss_train", train_loss/(batch_idx+1), iter)
+    writer.add_scalar("train_error",100.*(1.-correct/total),iter)
+    print(f"Iteraions : {iter}")
+    return iter 
+
+def test(epoch,iter):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+    writer.add_scalar("Loss_test", test_loss/(batch_idx+1), iter)
+    writer.add_scalar("test_error",100.*(1.-correct/total), iter)
+    
+iter = 0
+start_epoch = 0
+for epoch in range(start_epoch, start_epoch+200):
+    iter = train(epoch,iter)
+    test(epoch,iter)
+    if iter > 64000:
+        break
+    scheduler.step()
+writer.flush()
+writer.close()
